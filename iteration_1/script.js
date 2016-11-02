6d76323d9d79802089f7a776a24854a1efd1a746
Cristina Cepeda <cristinacepeda@Cristinas-MBP.wireless.newschool.edu>
2016-11-02 14:25:59 -0400

    initial commit

---
+++ iteration_1/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+//append new element 100 times
+for (i = 0; i < 100; i++) { 
+  $("body").append( "<div class='element' style='background-color: #b2b2b2;'></div>" );
+  console.log("element " + i + " added");
+}
+
+  //mouse interaction
+  $('.element').mouseover(function(){
+    $(this).css('background-color','black');
+  });
+
+  $('.element').mouseout(function(){
+    $(this).css('background-color','#ffbb99');
+  });
+});
